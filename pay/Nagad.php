658c16706dc692723cec54a56b3782ee027c6fd2
world9263 <world9263@users.noreply.github.com>
2026-08-30 20:01:32 +0530

    fix: force curl to maintain POST method and body through 302 redirects by setting CURLOPT_POSTREDIR to 3

--- pay/Nagad.php
+++ pay/Nagad.php
@@ -85,6 +85,7 @@ $ch = curl_init($apiUrl);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_POSTREDIR, 3);
 curl_setopt($ch, CURLOPT_MAXREDIRS, 5);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
