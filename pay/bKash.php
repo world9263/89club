9ba0f38063bfefca41b3cd75b5ee49998e8318fa
world9263 <world9263@users.noreply.github.com>
2026-08-30 19:57:49 +0530

    fix: enable CURLOPT_FOLLOWLOCATION in bKash and Nagad curl requests to resolve 302 Found redirects from gateway base URL

--- pay/bKash.php
+++ pay/bKash.php
@@ -84,6 +84,8 @@ $params['sign_type'] = 'MD5';
 $ch = curl_init($apiUrl);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_MAXREDIRS, 5);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/x-www-form-urlencoded']);
